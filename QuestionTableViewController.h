71262fab4488a73b98b377d87fe53b6b5991cfc7
Victor Hristoskov <victor_hristoskov@mail.bg>
2012-06-22 19:54:50 +0300

    Added QuestionTableViewController

---
+++ QuestionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionTableViewController.h
+//  Quizzy
+//
+//  Created by Victor Hristoskov on 6/22/12.
+//  Copyright (c) 2012 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionTableViewController : UITableViewController
+
+@end
